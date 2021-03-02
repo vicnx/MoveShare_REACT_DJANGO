4b811e4990166be2862401b28b2957fc74e274b3
vicnx <andanivicente@gmail.com>
2021-03-02 21:50:15 +0100

    some comments

--- frontend/src/services/login.jsx
+++ frontend/src/services/login.jsx
@@ -2,15 +2,16 @@ import API_URL from "../common/config";
 import createHttp from "./http";
 
 export default async function login(user) {
-  console.log(user);
+  //creamos una conexion sin necesidad de token
   let http = createHttp(false);
+
+  //realizamos la peticion de login
   return http
     .post(`${API_URL}users/login`, user)
     .then((res) => {
       return res.data.user;
     })
     .catch(({ response }) => {
-      console.log(response)
       return response.data.errors
     });
 }
