abd89716dc2745a0b0fbb795ef8cae5657bfd3ef
Vicente Andani <andanivicente@gmail.com>
2021-04-26 07:45:34 +0000

    clear code

--- frontend/src/pages/exercices/exercices.jsx
+++ frontend/src/pages/exercices/exercices.jsx
@@ -24,24 +24,6 @@ export default function Exercices() {
   }
   const {exercices,refreshExercices,loading} = useExercices({params},category);
 
-
-
-  // if(category){
-  //   exercices.filter(exercice=>{
-  //     exercice.categories.map((cat)=>{
-  //       console.log(cat.id);
-  //       console.log(category);
-  //       if(cat.id==category){
-  //         console.log("hola");
-  //         arr.push(exercice)
-  //       }
-  //     })
-  //   })
-  //   console.log(arr);
-  // }else{
-  //   arr = exercices
-  // }
-
   return (
       <Container className="home">
 
