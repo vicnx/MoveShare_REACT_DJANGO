72b36fb53c04fd9adc8dbb6a3b77928ea9127b47
Vicente Andani <andanivicente@gmail.com>
2021-04-26 07:44:36 +0000

    Fix exercice route

--- frontend/src/common/config.jsx
+++ frontend/src/common/config.jsx
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000/api/";
-// const API_URL = "https://8000-aqua-macaw-esz0q18v.ws-eu03.gitpod.io/api/";
+// const API_URL = "http://localhost:8000/api/";
+const API_URL = "https://8000-indigo-rabbit-ciz68y4l.ws-eu03.gitpod.io/api/";
 // const ROOT_URL=gp url 8000+"/api"
 export default API_URL;
